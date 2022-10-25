167da7ceb6fb1b883c1d0350752ec0856a5bdb06
haries <bongjoonkim96@gmail.com>
2022-10-26 00:10:47 +0900

    기존 개발 화면 복사

---
+++ src/components/contents/MainPage/MainPage.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import Button from "../../elements/Button/Button";
+
+function MainPage() {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    return (
+        <div className="container">
+            <div className="content-section"><h1>Cafeteria &amp; Office Catering Solutions Employees LOVE</h1>
+                <div className="description"> Corporate catering and cafeteria solutions customized to your employees’ unique
+                    tastes while delivering the flexibility and consistency large enterprises expect.
+                    <Button onClick={() => setModalIsOpen(true)}>
+                        Get Started Now
+                    </Button>
+                    <Button >
+                        안녕
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
