19cde66b9f0b55744941b44ae83c997d77d14ec2
bongjoonkim <bongjoonkim96@gmail.com>
2023-11-04 14:26:35 +0900

    백엔드 연결 로직 수정

--- src/services/request-response-service.ts
+++ src/services/request-response-service.ts
@@ -4,7 +4,7 @@ import axios, {AxiosRequestConfig} from "axios";
 const request = axios.create({
     baseURL: "", // 기본 서버 주소 입력
     headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:3001',
+        'Access-Control-Allow-Origin': 'http://13.125.108.196:3001',
         'Access-Control-Allow-Credentials' : false,
         "Content-Type": `application/json;charset=UTF-8`,
     },
