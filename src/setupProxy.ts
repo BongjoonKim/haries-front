6fc7eb5983b69dbe5b41a689343509e898b39a3a
bongjoonkim <bongjoonkim96@gmail.com>
2023-11-20 22:47:11 +0900

    ec2 신규 환경 적용

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -3,7 +3,7 @@ export const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app:any) {
   app.use(
     createProxyMiddleware("/*", {
-      target: 'http://3.35.231.184:3001',
+      target: 'http://3.35.214.21:3001',
       changeOrigin: true,
     })
   );
