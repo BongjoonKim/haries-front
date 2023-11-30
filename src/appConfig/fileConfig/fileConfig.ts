dd58256531f4709828a8a2baff1d482ce57c123b
bongjoonkim <bongjoonkim96@gmail.com>
2023-11-30 09:06:01 +0900

    파일 config 수정

--- src/appConfig/fileConfig/fileConfig.ts
+++ src/appConfig/fileConfig/fileConfig.ts
@@ -1,10 +1,12 @@
-import AWS from "aws-sdk";
-import S3 from "react-aws-s3-typescript";
+// import AWS from "aws-sdk";
+import {S3Client, PutObjectCommand} from "@aws-sdk/client-s3";
+// import S3 from "react-aws-s3-typescript";
 import generatorUtil from "../../utilities/generatorUtil";
 
-interface fileS3ConfigPropsㅇ {
+interface fileS3ConfigProps {
   blob : Blob,
-  
+  documentId ?: string | "new";
+  type ?: "upload" | "delete" | "find";
   
 }
 
@@ -13,26 +15,45 @@ export default async function fileS3Config(props : fileS3ConfigProps) {
   const ACCESS_KEY_ID = `${process.env.REACT_APP_AWS_ACCESS_KEY_ID}`!;
   const SECRET_ACCESS_KEY_ID = `${process.env.REACT_APP_AWS_SECRET_ACCESS_KEY_ID}`!;
   
-  const config = {
-    bucketName: "haries-img",
+  const client = new S3Client({
     region: REGION,
-    accessKeyId : ACCESS_KEY_ID,
-    secretAccessKey : SECRET_ACCESS_KEY_ID
+    credentials: {
+      accessKeyId: ACCESS_KEY_ID,
+      secretAccessKey: SECRET_ACCESS_KEY_ID,
+  });
+  const file = new File([props.blob], generatorUtil.uuid(), {type:props.blob.type})
+  const params = {
+    Bucket: "haries-img",
+    Key: '/haries',
+    Body: file,
   };
   
-  const file = new File([props.blob], generatorUtil.uuid(), {type:props.blob.type})
+  const command = new PutObjectCommand(params);
+  const response = await client.send(command);
   
-  const S3Client = new S3(config);
-  // 데이터 조회는 잘 됨
-  const data = await S3Client.listFiles();
-  console.log("데이터 확인", data);
+  console.log("response", response)
   
-  const formData = new FormData();
-  formData.append("file", file);
+  // const config = {
+  //   bucketName: "haries-img",
+  //   region: REGION,
+  //   accessKeyId : ACCESS_KEY_ID,
+  //   secretAccessKey : SECRET_ACCESS_KEY_ID,
+  //   dirName: props.documentId ? props.documentId : "new"
+  // };
+  //
+  // const file = new File([props.blob], generatorUtil.uuid(), {type:props.blob.type})
+  //
+  // const S3Client = new S3(config);
+  // // 데이터 조회는 잘 됨
+  // const data = await S3Client.listFiles();
+  // console.log("데이터 확인", data);
+  //
+  // const formData = new FormData();
+  // formData.append("file", file);
+  //
+  //
+  // const response = await S3Client.uploadFile(file, "ㅇㅇㅇㅇ");
+  // console.log("리스폰스", response.location)
   
-  
-  const response = await S3Client.uploadFile(file, "ㅇㅇㅇㅇ");
-  console.log("리스폰스", response.location)
-  
-  return response.location;
+  return response.;
 }
