5075eb6fac3b48ca9d5d5695a7d989c239882f7f
bongjoonkim <bongjoonkim96@gmail.com>
2024-02-05 01:14:16 +0900

    챗지피티 markdown 개발 완료

--- src/components/widgets/MarkDown/MarkDown.tsx
+++ src/components/widgets/MarkDown/MarkDown.tsx
@@ -3,6 +3,7 @@ import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import styled from "styled-components";
 
 
 interface MarkdownProps {
@@ -11,12 +12,31 @@ interface MarkdownProps {
 
 function MarkDown(props : MarkdownProps) {
   return (
-      <ReactMarkdown
+      <StyledReactMarkdown
         remarkPlugins={[remarkGfm]}
         children={props.contents}
-        rehypePlugins={[rehypeHighlight]}
+        // rehypePlugins={[rehypeHighlight]}
+        components={{
+          code({node, className, children, ...props}) {
+            const match = /language-(\w+)/.exec(className || "");
+            return (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          }
+        }}
       />
   )
 }
 
 export default MarkDown;
+
+const StyledReactMarkdown = styled(ReactMarkdown)`
+  pre {
+    background: rgb(60 59 59);
+    padding: 1rem;
+    border-radius: 10px;
+    color: snow;
+  }
+`
