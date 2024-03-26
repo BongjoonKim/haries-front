434ee6771634a6334c1c6f713b2d9cb67dd70324
bongjoonkim <bongjoonkim96@gmail.com>
2024-03-26 19:44:04 +0900

    Dalle 목록 불러오기 추가

--- src/pages/dalle/layout/QuestionBox/useQuestionBox.ts
+++ src/pages/dalle/layout/QuestionBox/useQuestionBox.ts
@@ -1,9 +1,10 @@
 import {useCallback} from "react";
+import {getDalleImages} from "../../../../endpoints/dalle-endpoints";
 
 function useQuestionBox(props ?: any) {
   const handleEnter = useCallback(async (event : any) => {
     if (event.code === "Enter") {
-    
+      await getDalleImages()
     }
   }, []);
   
