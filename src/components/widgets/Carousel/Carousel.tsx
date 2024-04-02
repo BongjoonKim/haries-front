99d6d5531ee2d587965b2f26f1f906a20cb5a248
bongjoonkim <bongjoonkim96@gmail.com>
2024-04-03 08:52:06 +0900

    carousel 개발 전 구동 원리 학습 중

---
+++ src/components/widgets/Carousel/Carousel.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export interface CarouselProps {
+
+}
+export default function Carousel(props : CarouselProps) {
+  return (
+    <StyledCarousel>
+    
+    </StyledCarousel>
+  )
+}
+
+const StyledCarousel = styled.div`
+`;
