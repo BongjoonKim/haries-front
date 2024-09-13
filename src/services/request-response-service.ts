fc08e8f09c4a1be7ce419311b4f56ee4daa3b19b
bongjoonkim <bongjoonkim96@gmail.com>
2024-09-13 23:42:57 +0900

    backend 연결 주소를 env 파일에서 가져오도록 작업

--- src/services/request-response-service.ts
+++ src/services/request-response-service.ts
@@ -5,7 +5,7 @@ const request = axios.create({
     baseURL: "", // 기본 서버 주소 입력
     headers: {
         // 'Access-Control-Allow-Origin': 'http://13.209.27.28:3001',
-        'Access-Control-Allow-Origin': 'http://localhost:3001',
+        'Access-Control-Allow-Origin': `${process.env.REACT_APP_BACKEND_URI}`,
         'Access-Control-Allow-Credentials' : true,
         "Content-Type": `application/json;charset=UTF-8`,
     },
@@ -15,7 +15,7 @@ const request = axios.create({
 export const securityReq = axios.create({
     baseURL:"/",
     headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:3001/',
+        'Access-Control-Allow-Origin': `${process.env.REACT_APP_BACKEND_URI}`,
         'Access-Control-Allow-Credentials' : true,
         'Content-Type': 'application/x-www-form-urlencoded'
     }
